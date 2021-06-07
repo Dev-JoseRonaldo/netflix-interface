3c84dc7bcfc01bc6cfa00c836f7948f4f1d08d85
José Ronaldo <dev.joseronaldo@gmail.com>
2021-06-07 18:42:01 -0300

    carousel usando jquery

---
+++ assets/js/owl/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:15,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:5
+        },
+        1000:{
+            items:6
+        }
+    }
+})
